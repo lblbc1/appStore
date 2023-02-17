d68f90abdd326fe7b7dbf1ca625a637688eaee71
jerry <309032663@qq.com>
2023-02-17 22:03:07 +0800

    update

--- appStore_client/appStore_vue_vant/src/api/api.ts
+++ appStore_client/appStore_vue_vant/src/api/api.ts
@@ -2,16 +2,16 @@ import axiosInstance, { AxiosResponseProps } from '@/uitls/request';
 
 export const queryCategories = () => {
 	return axiosInstance.get('appstore/categories');
-};
+}
 
 export const queryAppsByCategory = (categoryId: string) => {
 	return axiosInstance.get('appstore/apps?categoryId='+categoryId);
-};
+}
 
 export const queryApp = (blogId: string) => {
 	return axiosInstance.get('appstore/app/'+blogId);
-};
+}
 
 export function search(keyword) {
 	return axiosInstance.get(`appstore/appsBySearch?keyword=${keyword}`);
-  }
+}
