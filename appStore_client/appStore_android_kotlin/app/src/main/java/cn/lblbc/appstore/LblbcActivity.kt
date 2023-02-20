b3aa36494eb95ecf850d8ce9ab40c2fd4473859d
jerry <309032663@qq.com>
2023-02-20 18:24:05 +0800

    update

--- appStore_client/appStore_android_kotlin/app/src/main/java/cn/lblbc/appstore/LblbcActivity.kt
+++ appStore_client/appStore_android_kotlin/app/src/main/java/cn/lblbc/appstore/LblbcActivity.kt
@@ -9,7 +9,7 @@ import cn.lblbc.appstore.module.home.HomeFragment
 import cn.lblbc.appstore.module.mine.MineFragment
 import cn.lblbc.lib.utils.ToastUtil
 import com.permissionx.guolindev.PermissionX
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_lblbc.*
 
 /**
  * 厦门大学计算机专业 | 前华为工程师
@@ -17,12 +17,12 @@ import kotlinx.android.synthetic.main.activity_main.*
  * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
  * 公众号：蓝不蓝编程
  */
-class MainActivity : AppCompatActivity() {
+class LblbcActivity : AppCompatActivity() {
     private var lastIndex = 0
     private var mFragments = mutableListOf<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lblbc)
         initBottomNavigation()
         initFragments()
         applyPermissionForDownload()
